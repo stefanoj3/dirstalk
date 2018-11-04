008073261b323697d0213627c0ba252195b27b52
Stefano Gabryel <stefanoj3@users.noreply.github.com>
2018-11-04 19:11:24 +0100

    Adding support for scan depth (#6)

--- pkg/scan/reproducer.go
+++ pkg/scan/reproducer.go
@@ -1,23 +1,57 @@
 package scan
 
-import "github.com/chuckpreslar/emission"
+import (
+	"path"
+	"sync"
+
+	"github.com/chuckpreslar/emission"
+	"github.com/stefanoj3/dirstalk/pkg/pathutil"
+)
 
 var statusCodesToSkip = map[int]bool{
 	404: false,
 }
 
 type ReProcessor struct {
-	eventEmitter *emission.Emitter
-	dictionary   []string
+	eventEmitter   *emission.Emitter
+	httpMethods    []string
+	dictionary     []string
+	resultRegistry sync.Map
 }
 
-func (r *ReProcessor) Process(result *Result) {
+func NewReProcessor(eventEmitter *emission.Emitter, httpMethods []string, dictionary []string) *ReProcessor {
+	return &ReProcessor{eventEmitter: eventEmitter, httpMethods: httpMethods, dictionary: dictionary}
+}
+
+func (r *ReProcessor) ReProcess(result *Result) {
 	if _, ok := statusCodesToSkip[result.Response.StatusCode]; ok {
 		return
 	}
 
-	// process directories
-	// process hidden files (eg .env)
-	// process hidden folders (.git)
-	// process files with extensions (eg index.php)
+	if result.Target.Depth <= 0 {
+		return
+	}
+
+	// no point in appending to a filename
+	if pathutil.HasExtension(result.Target.Path) == true {
+		return
+	}
+
+	_, inRegistry := r.resultRegistry.Load(result.Target.Path)
+	if inRegistry {
+		return
+	}
+	r.resultRegistry.Store(result.Target.Path, false)
+
+	for _, entry := range r.dictionary {
+		for _, httpMethod := range r.httpMethods {
+			t := result.Target
+			t.Depth -= 1
+			t.Path = path.Join(t.Path, entry)
+			t.Method = httpMethod
+
+			r.eventEmitter.Emit(EventTargetProduced, t)
+		}
+
+	}
 }
