5c28f12b9fced7a0ac66261aabf1e733ff74b6e3
Stefano Gabryel <esse.gab2@gmail.com>
2019-08-25 11:35:49 +0200

    Changing functional tests to make them fail when the exit code is not the expected one

--- functional-tests.sh
+++ functional-tests.sh
@@ -66,7 +66,7 @@ assert_contains "$VERSION_RESULT" "Built" "the build time is expected to be prin
 SCAN_RESULT=$(./dist/dirstalk scan 2>&1 || true);
 assert_contains "$SCAN_RESULT" "error" "an error is expected when no argument is passed"
 
-SCAN_RESULT=$(./dist/dirstalk scan -d resources/tests/dictionary.txt http://localhost:8080 2>&1 || true);
+SCAN_RESULT=$(./dist/dirstalk scan -d resources/tests/dictionary.txt http://localhost:8080 2>&1);
 assert_contains "$SCAN_RESULT" "/index" "result expected when performing scan"
 assert_contains "$SCAN_RESULT" "/index/home" "result expected when performing scan"
 assert_contains "$SCAN_RESULT" "3 results found" "a recap was expected when performing a scan"
@@ -76,7 +76,7 @@ assert_contains "$SCAN_RESULT" "    └── home" "a recap was expected when p
 
 assert_not_contains "$SCAN_RESULT" "error" "no error is expected for a successful scan"
 
-SCAN_RESULT=$(./dist/dirstalk scan -h 2>&1 || true);
+SCAN_RESULT=$(./dist/dirstalk scan -h 2>&1);
 assert_contains "$SCAN_RESULT" "\-\-dictionary" "dictionary help is expected to be printed"
 assert_contains "$SCAN_RESULT" "\-\-cookie" "cookie help is expected to be printed"
 assert_contains "$SCAN_RESULT" "\-\-header" "header help is expected to be printed"
@@ -91,18 +91,18 @@ assert_contains "$SCAN_RESULT" "\-\-scan-depth" "scan-depth help is expected to 
 
 assert_not_contains "$SCAN_RESULT" "error" "no error is expected when priting scan help"
 
-DICTIONARY_GENERATE_RESULT=$(./dist/dirstalk dictionary.generate resources/tests 2>&1 || true);
+DICTIONARY_GENERATE_RESULT=$(./dist/dirstalk dictionary.generate resources/tests 2>&1);
 assert_contains "$DICTIONARY_GENERATE_RESULT" "dictionary.txt" "dictionary generation should contains a file in the folder"
 assert_not_contains "$DICTIONARY_GENERATE_RESULT" "error" "no error is expected when generating a dictionary successfully"
 
-RESULT_VIEW_RESULT=$(./dist/dirstalk result.view -r resources/tests/out.txt 2>&1 || true);
+RESULT_VIEW_RESULT=$(./dist/dirstalk result.view -r resources/tests/out.txt 2>&1);
 assert_contains "$RESULT_VIEW_RESULT" "├── adview" "result output should contain tree output"
 assert_contains "$RESULT_VIEW_RESULT" "├── partners" "result output should contain tree output"
 assert_contains "$RESULT_VIEW_RESULT" "│   └── terms" "result output should contain tree output"
 assert_contains "$RESULT_VIEW_RESULT" "└── s" "result output should contain tree output"
 assert_not_contains "$RESULT_VIEW_RESULT" "error" "no error is expected when displaying a result"
 
-RESULT_DIFF_RESULT=$(./dist/dirstalk result.diff -f resources/tests/out.txt -s resources/tests/out2.txt 2>&1 || true);
+RESULT_DIFF_RESULT=$(./dist/dirstalk result.diff -f resources/tests/out.txt -s resources/tests/out2.txt 2>&1);
 assert_contains "$RESULT_DIFF_RESULT" "├── adview" "result output should contain diff"
 assert_contains "$RESULT_DIFF_RESULT" "├── partners" "result output should contain diff"
 assert_contains "$RESULT_DIFF_RESULT" $(echo "│   └── \x1b[31mterms\x1b[0m\x1b[32m123\x1b[0m") "result output should contain diff"
